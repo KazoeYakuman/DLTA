ed7e6028c9ebdfef4f43c0de95e943cc07fb03e0
kazoeyakuman <liujiaming@pku.edu.cn>
2024-04-18 15:24:07 +0800

    hw1 done

--- hw1/lr_opt/InverseTimeDecay.py
+++ hw1/lr_opt/InverseTimeDecay.py
@@ -1,7 +1,10 @@
 
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+
+np.random.seed(306)
 
 
 def load_data(filename):
@@ -19,15 +22,15 @@ def load_label(filename):
     with open(filename, 'rb') as f:
         magic = int.from_bytes(f.read(4), byteorder='big')
         assert magic == 2049, 'Invalid magic number ' + str(magic)
-        # num_labels = int.from_bytes(f.read(4), byteorder='big')
+        num_labels = int.from_bytes(f.read(4), byteorder='big')
         labels = np.frombuffer(f.read(), dtype=np.uint8)
     return labels
 
 
-train_data_path = './data/train_data'
-train_label_path = './data/train_label'
-test_data_path = './data/test_data'
-test_label_path = './data/test_label'
+train_data_path = '../data/train_data'
+train_label_path = '../data/train_label'
+test_data_path = '../data/test_data'
+test_label_path = '../data/test_label'
 train_data = load_data(train_data_path)
 train_label = load_label(train_label_path)
 test_data = load_data(test_data_path)
@@ -46,11 +49,11 @@ class MLP:
         self.input_size = input_size
         self.output_size = output_size
         self.W1 = np.random.randn(input_size, 256)
-        self.W2 = np.random.randn(256, 64)
-        self.W3 = np.random.randn(64, output_size)
+        self.W2 = np.random.randn(256, output_size)
         self.b1 = np.zeros(256)
-        self.b2 = np.zeros(64)
-        self.b3 = np.zeros(output_size)
+        self.b2 = np.zeros(output_size)
+        self.loss = []
+        self.beta = 0.1
 
     def softmax(self, x):
         x = x - np.max(x, axis=-1, keepdims=True)
@@ -64,44 +67,41 @@ class MLP:
 
     def train(self, X, y, epoch=100, lr=0.2):
         for e in tqdm(range(epoch)):
+            tlr = lr / (1 + self.beta * e)
             h1 = X @ self.W1 + self.b1
             h1 = self.relu(h1)
-            h2 = h1 @ self.W2 + self.b2
-            h2 = self.relu(h2)
-            y_pred = h2 @ self.W3 + self.b3
+            y_pred = h1 @ self.W2 + self.b2
             y_pred = self.softmax(y_pred)
-            loss = self.cross_entropy(y_pred, y)
-            if e % 10 == 9:
-                print(loss)
+            self.loss.append(self.cross_entropy(y_pred, y))
             dy = y_pred - y
-            dW3 = h2.T @ dy / X.shape[0]
-            db3 = np.sum(dy, axis=0) / X.shape[0]
-            dh2 = dy @ self.W3.T * (h2 > 0) / X.shape[0]
-            dW2 = h1.T @ dh2 / X.shape[0]
-            db2 = np.sum(dh2, axis=0) / X.shape[0]
-            dh1 = dh2 @ self.W2.T * (h1 > 0) / X.shape[0]
+            dW2 = h1.T @ dy / X.shape[0]
+            db2 = np.sum(dy, axis=0) / X.shape[0]
+            dh1 = dy @ self.W2.T * (h1 > 0) / X.shape[0]
             dW1 = X.T @ dh1 / X.shape[0]
             db1 = np.sum(dh1, axis=0) / X.shape[0]
-            self.W3 -= lr * dW3
-            self.b3 -= lr * db3
-            self.W2 -= lr * dW2
-            self.b2 -= lr * db2
-            self.W1 -= lr * dW1
-            self.b1 -= lr * db1
+            self.W2 -= tlr * dW2
+            self.b2 -= tlr * db2
+            self.W1 -= tlr * dW1
+            self.b1 -= tlr * db1
 
     def test(self, X, y):
         h1 = X @ self.W1 + self.b1
         h1 = self.relu(h1)
-        h2 = h1 @ self.W2 + self.b2
-        h2 = self.relu(h2)
-        y_pred = h2 @ self.W3 + self.b3
+        y_pred = h1 @ self.W2 + self.b2
         y_pred = self.softmax(y_pred)
         acc = np.mean(np.argmax(y_pred, axis=-1) == np.argmax(y, axis=-1))
-        print(acc)
+        return acc
+
+    def plot_loss(self, fig_name, acc):
+        xs = range(1, len(self.loss) + 1)
+        plt.plot(xs, self.loss, 'b', label='Loss')
+        plt.title(f'Loss(test_acc={acc:.4f})')
+        plt.xlabel('')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.savefig(fig_name)
 
 
 model = MLP(784, 10)
-model.train(train_data, train_label_one_hot, epoch=100, lr=0.01)
-model.test(test_data, test_label_one_hot)
-model.test()
-time.sleep(999)
+model.train(train_data, train_label_one_hot, epoch=500, lr=0.12)
+model.plot_loss('InverseTimeDecay.png', model.test(test_data, test_label_one_hot))
--- hw1/regularization/L1.py
+++ hw1/regularization/L1.py
@@ -1,7 +1,10 @@
 
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+
+np.random.seed(306)
 
 
 def load_data(filename):
@@ -19,15 +22,15 @@ def load_label(filename):
     with open(filename, 'rb') as f:
         magic = int.from_bytes(f.read(4), byteorder='big')
         assert magic == 2049, 'Invalid magic number ' + str(magic)
-        # num_labels = int.from_bytes(f.read(4), byteorder='big')
+        num_labels = int.from_bytes(f.read(4), byteorder='big')
         labels = np.frombuffer(f.read(), dtype=np.uint8)
     return labels
 
 
-train_data_path = './data/train_data'
-train_label_path = './data/train_label'
-test_data_path = './data/test_data'
-test_label_path = './data/test_label'
+train_data_path = '../data/train_data'
+train_label_path = '../data/train_label'
+test_data_path = '../data/test_data'
+test_label_path = '../data/test_label'
 train_data = load_data(train_data_path)
 train_label = load_label(train_label_path)
 test_data = load_data(test_data_path)
@@ -46,11 +49,11 @@ class MLP:
         self.input_size = input_size
         self.output_size = output_size
         self.W1 = np.random.randn(input_size, 256)
-        self.W2 = np.random.randn(256, 64)
-        self.W3 = np.random.randn(64, output_size)
+        self.W2 = np.random.randn(256, output_size)
         self.b1 = np.zeros(256)
-        self.b2 = np.zeros(64)
-        self.b3 = np.zeros(output_size)
+        self.b2 = np.zeros(output_size)
+        self.loss = []
+        self.lbd = 0.0001
 
     def softmax(self, x):
         x = x - np.max(x, axis=-1, keepdims=True)
@@ -66,42 +69,40 @@ class MLP:
         for e in tqdm(range(epoch)):
             h1 = X @ self.W1 + self.b1
             h1 = self.relu(h1)
-            h2 = h1 @ self.W2 + self.b2
-            h2 = self.relu(h2)
-            y_pred = h2 @ self.W3 + self.b3
+            y_pred = h1 @ self.W2 + self.b2
             y_pred = self.softmax(y_pred)
-            loss = self.cross_entropy(y_pred, y)
-            if e % 10 == 9:
-                print(loss)
+            self.loss.append(self.cross_entropy(y_pred, y) + self.lbd * (np.sum(np.abs(self.W1)) + np.sum(np.abs(self.W2))))
             dy = y_pred - y
-            dW3 = h2.T @ dy / X.shape[0]
-            db3 = np.sum(dy, axis=0) / X.shape[0]
-            dh2 = dy @ self.W3.T * (h2 > 0) / X.shape[0]
-            dW2 = h1.T @ dh2 / X.shape[0]
-            db2 = np.sum(dh2, axis=0) / X.shape[0]
-            dh1 = dh2 @ self.W2.T * (h1 > 0) / X.shape[0]
+            dW2 = h1.T @ dy / X.shape[0]
+            db2 = np.sum(dy, axis=0) / X.shape[0]
+            dh1 = dy @ self.W2.T * (h1 > 0) / X.shape[0]
             dW1 = X.T @ dh1 / X.shape[0]
             db1 = np.sum(dh1, axis=0) / X.shape[0]
-            self.W3 -= lr * dW3
-            self.b3 -= lr * db3
+            self.W2 -= lr * self.lbd * np.sign(self.W2)
             self.W2 -= lr * dW2
             self.b2 -= lr * db2
+            self.W1 -= lr * self.lbd * np.sign(self.W1)
             self.W1 -= lr * dW1
             self.b1 -= lr * db1
 
     def test(self, X, y):
         h1 = X @ self.W1 + self.b1
         h1 = self.relu(h1)
-        h2 = h1 @ self.W2 + self.b2
-        h2 = self.relu(h2)
-        y_pred = h2 @ self.W3 + self.b3
+        y_pred = h1 @ self.W2 + self.b2
         y_pred = self.softmax(y_pred)
         acc = np.mean(np.argmax(y_pred, axis=-1) == np.argmax(y, axis=-1))
-        print(acc)
+        return acc
+
+    def plot_loss(self, fig_name, acc):
+        xs = range(1, len(self.loss) + 1)
+        plt.plot(xs, self.loss, 'b', label='Loss')
+        plt.title(f'Loss(test_acc={acc:.4f})')
+        plt.xlabel('')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.savefig(fig_name)
 
 
 model = MLP(784, 10)
-model.train(train_data, train_label_one_hot, epoch=100, lr=0.01)
-model.test(test_data, test_label_one_hot)
-model.test()
-time.sleep(999)
+model.train(train_data, train_label_one_hot, epoch=500, lr=0.12)
+model.plot_loss('L1.png', model.test(test_data, test_label_one_hot))
